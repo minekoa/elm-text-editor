db2e2334805ee2b309df6e0d84e244ff3968f626
minekoa <mineko.orange@gmail.com>
2018-05-26 00:50:10 +0900

    テスト追加 マーク更新 編集 レンジ削除

--- tests/BufferMarkSetTest.elm
+++ tests/BufferMarkSetTest.elm
@@ -314,11 +314,35 @@ suite =
                       |> Buffer.moveAt (2, 2)
                       |> Buffer.markSet
                       |> Buffer.markClear 
+                      |> Buffer.deleteAt (0, 3)
+                      |> Expect.all [ \m -> Expect.equal "ABCDEF\nGHmIJ\nK\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (1,2) False  |> Just) m.mark
+                                    ]
+
+
+        , test "mark-update by delete one (before row, concat before and mark line)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nK\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
                       |> Buffer.deleteAt (1, 3)
-                      |> .mark
-                      |> Expect.equal (Buffer.Mark (1, 2) False |> Just)
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEFGHmIJ\nK\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,1) False  |> Just) m.mark
+                                    ]
 
         , test "mark-update by delete one (just row, before column, 1char)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nK\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteAt (2, 0)
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nHmIJ\nK\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,1) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete one (just row, before column, 1char (pre-char))" <|
               \_ ->
                   Buffer.init "ABC\nDEF\nGHmIJ\nK\n"
                       |> Buffer.moveAt (2, 2)
@@ -373,7 +397,139 @@ suite =
                       |> Expect.equal (Buffer.Mark (2, 2) False |> Just)
 
 
+        -- update mark by delete range
 
+        , test "mark-update by delete range (before row, inline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (1, 0) (1, 2))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nF\nGHmIJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2, 2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (before row, multiline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (0, 1) (1, 1))
+                      |> Expect.all [ \m -> Expect.equal "AEF\nGHmIJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (1,2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (before row, delete -> concat before and markline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (0, 1) (2, 0))
+                      |> Expect.all [ \m -> Expect.equal "AGHmIJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (0,3) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (row, multiline, contain mark)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (0, 1) (3, 1))
+                      |> Expect.all [ \m -> Expect.equal "ALM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (0,1) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, before column, 1char)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 0) (2, 2))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nmIJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,0) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, inline, contain mark)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 1) (2, 4))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,1) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, before column, multiline, contain mark)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 1) (3, 1))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,1) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, just column, 1char)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 2) (2, 3))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGHIJ\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, just column, multiline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 2) (3, 1))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGHLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, after column, inline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 3) (2, 5))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGHm\nKLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (just row, after column, multiline)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (2, 3) (3, 1))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGHmLM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,2) False  |> Just) m.mark
+                                    ]
+
+        , test "mark-update by delete range (after row)" <|
+              \_ ->
+                  Buffer.init "ABC\nDEF\nGHmIJ\nKLM\n"
+                      |> Buffer.moveAt (2, 2)
+                      |> Buffer.markSet
+                      |> Buffer.markClear 
+                      |> Buffer.deleteRange (Buffer.Range (3, 0) (3, 2))
+                      |> Expect.all [ \m -> Expect.equal "ABC\nDEF\nGHmIJ\nM\n" (m.contents |> String.join "\n")
+                                    , \m -> Expect.equal (Buffer.Mark (2,2) False  |> Just) m.mark
+                                    ]
         ]
 
 
