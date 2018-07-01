1fb5020229c65065e5f54d8c142670d3e2c8f454
minekoa <mineko.orange@gmail.com>
2018-07-02 00:09:32 +0900

    example をdocs に追加など

--- example/markdown/Main.elm
+++ example/markdown/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import TextEditor
 import TextEditor.KeyBind
+import TextEditor.Style
+import TextEditor.Option
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -28,6 +30,32 @@ main =
         }
 
 
+defaultText : String
+defaultText =
+    "# Realtime markdown editor (expampke)\n\n* lv1\n* lv1\n    * lv2\n\nabcd efg \nhijk lmn\n\n    opqr stu\n\n> vw\nxyz\n\n```\n01234 567\n```\n\n|果物|個数|\n|----|----|\n|りんご|1|\n|ばなな!|∞|\n"
+
+
+styleSetting : TextEditor.Style.Style -> TextEditor.Style.Style
+styleSetting sty =
+    { sty
+        | common =
+            Just
+                { color = "snow"
+                , backgroundColor = "dimgray"
+                , opacity = ""
+                , fontFamily = "monospace"
+                , fontSize = "1.2em"
+                }
+    }
+
+
+optionSetting : TextEditor.Option.Option -> TextEditor.Option.Option
+optionSetting opt =
+    { opt
+        | showControlCharactor = True
+    }
+
+
 init : ( Model, Cmd Msg )
 init =
     let
@@ -35,17 +63,14 @@ init =
             (TextEditor.init
                 "editor-id1"
                 (TextEditor.KeyBind.basic ++ TextEditor.KeyBind.gates ++ TextEditor.KeyBind.emacsLike)
-                "foobar hogehoge"
-            ) |> (\(m,c) ->
-                      let
-                          sty = m.style
-                      in
-                          ( { m | style =
-                                { sty | common = Just { color="snow", backgroundColor="dimgray", opacity="", fontFamily="monospace", fontSize="1.2em"} }
-                            }
-                          , c
-                          )
-                 )
+                defaultText
+            )
+                |> (\( m, c ) ->
+                        ( { m | style = styleSetting m.style }
+                            |> \m -> TextEditor.setOptions (optionSetting (TextEditor.options m)) m
+                        , c
+                        )
+                   )
     in
         ( Model m (TextEditor.buffer m).contents
         , Cmd.map EditorMsg c
@@ -62,7 +87,8 @@ update msg model =
             in
                 case edmsg of
                     TextEditor.UpdateContents contents ->
-                        ( { model | editor = m , contents = contents }, Cmd.map EditorMsg c )
+                        ( { model | editor = m, contents = contents }, Cmd.map EditorMsg c )
+
                     _ ->
                         ( { model | editor = m }, Cmd.map EditorMsg c )
 
@@ -74,23 +100,22 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [ style [ ("display", "flex")
-                , ("width", "100%")
-                , ("height", "100%")
-                ]
-        ] 
-        [ div [ style [("height", "100%"), ("width", "50%")] ] [ Html.map EditorMsg (TextEditor.view model.editor) ]
-        , div [ style [("height", "100%"), ("width", "50%")] ] [ Markdown.toHtmlWith markdownOptions [class "md"] (String.join "\n" model.contents) ]
+    div
+        [ style
+            [ ( "display", "flex" )
+            , ( "width", "100%" )
+            , ( "height", "100%" )
+            ]
+        ]
+        [ div [ style [ ( "height", "100%" ), ( "width", "50%" ) ] ] [ Html.map EditorMsg (TextEditor.view model.editor) ]
+        , div [ style [ ( "height", "100%" ), ( "width", "50%" ) ] ] [ Markdown.toHtmlWith markdownOptions [ class "md" ] (String.join "\n" model.contents) ]
         ]
 
 
-
-
-markdownOptions: Markdown.Options
+markdownOptions : Markdown.Options
 markdownOptions =
-  { githubFlavored = Just { tables = True, breaks = True }
-  , defaultHighlighting = Nothing
-  , sanitize = False
-  , smartypants = False
-  }
-
+    { githubFlavored = Just { tables = True, breaks = True }
+    , defaultHighlighting = Nothing
+    , sanitize = False
+    , smartypants = False
+    }
